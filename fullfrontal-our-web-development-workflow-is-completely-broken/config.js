cbe685bbe9f5417a3322be8847715a7ad6f304e3
Kenneth Auchenberg <kenneth@auchenberg.dk>
2013-11-10 02:32:57 +0000

    Added FullFrontal 2013 "Our web development workflow is completely broken" slides.

---
+++ fullfrontal-our-web-development-workflow-is-completely-broken/config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Our web development workflow is completely broken.',
+    subtitle: 'November 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Kenneth Auchenberg',
+    company: 'Front-end lead, Podio</br>Citrix SaaS Division ',
+    gplus: '',
+    twitter: '@auchenberg',
+    www: 'http://kenneth.io',
+    github: ''
+  }]
+};
+
