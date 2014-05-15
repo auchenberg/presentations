c842544399a56b85d032bb14030ad1fb7d6abbe4
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-05-15 17:05:49 +0200

    Added "DevDK Where's the platform headed" talk

---
+++ devdk-wheres-the-webplatform-headed/config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: "Where's the webplatform headed?",
+    subtitle: 'ConDevDK, May 2014',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Kenneth Auchenbeg',
+    company: 'Tech lead, Hutt & G2M Free, Citrix',
+    gplus: '',
+    twitter: '@auchenberg',
+    www: 'http://kenneth.io',
+    github: ''
+  }]
+};
+
