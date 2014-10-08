39bf3ef7c3bbc4ce821792d0590709cd8a263f52
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-10-09 00:15:49 +0200

    Added "fronteers-2014-remotedebug-everything" talk.

---
+++ fronteers-2014-remotedebug-everything/config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'RemoteDebug everything!',
+    subtitle: 'Fronteers, Oktober 2014',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Kenneth Auchenberg',
+    company: 'Tech lead, Citrix GoToMeeting Free',
+    gplus: '',
+    twitter: '@auchenberg',
+    www: 'http://kenneth.io',
+    github: ''
+  }]
+};
+
